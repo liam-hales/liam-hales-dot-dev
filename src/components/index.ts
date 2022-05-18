db1d363cbd7d3016c40317f7827d09c2e85ca7a1
Liam Hales <liam.hales96@gmail.com>
2022-05-18 23:08:02 +0100

    Added Nav component which controls the navigation for the app

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
 import App from './app/app';
 import Content from './content/content';
+import Nav from './nav/nav';
 import TopNav from './nav/top/topNav';
 import BottomNav from './nav/bottom/bottomNav';
 
 export {
   App,
   Content,
+  Nav,
   TopNav,
   BottomNav,
 };
---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent, ReactElement } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { BaseProps } from '../../types';
+import { ScreenSize } from '../../enums';
+import { TopNav, BottomNav } from '..';
+import { useScreen } from '../../hooks';
+
+/**
+ * The `Nav` component props
+ */
+type Props = BaseProps;
+
+/**
+ * Controls the navigation for the app and what navigation
+ * component should be rendered depending on the screen size
+ *
+ * @param props The component props
+ * @returns The `Nav` component
+ */
+const Nav: FunctionComponent<Props> = ({ children }): ReactElement<Props> => {
+
+  const { screenSize } = useScreen();
+  return (
+    <BrowserRouter>
+      {
+        (screenSize !== ScreenSize.EXTRA_SMALL) && (
+          <TopNav />
+        )
+      }
+      {children}
+      {
+        (screenSize === ScreenSize.EXTRA_SMALL) && (
+          <BottomNav />
+        )
+      }
+    </BrowserRouter>
+  );
+};
+
+export default Nav;

6a7d077ea80259b35a906793986d2058f205b1f6
Liam Hales <liam.hales96@gmail.com>
2022-05-31 22:33:13 +0100

    Added useContext hook to get the current context value

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,11 @@
 import useConfig from './useConfig.hook';
+import useContext from './useContext.hook';
 import useNav from './useNav.hook';
 import useScreen from './useScreen.hook';
 
 export {
   useConfig,
+  useContext,
   useNav,
   useScreen,
 };

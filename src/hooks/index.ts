102742846fd32f77f4098f628b3be8e38133cdcb
Liam Hales <liam.hales96@gmail.com>
2022-06-05 22:14:07 +0100

    Added useQuery hook used to make GraphQL requests to the API to retrieve data

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,10 +2,12 @@ import useConfig from './useConfig.hook';
 import useContext from './useContext.hook';
 import useNav from './useNav.hook';
 import useScreen from './useScreen.hook';
+import useQuery from './useQuery.hook';
 
 export {
   useConfig,
   useContext,
   useNav,
   useScreen,
+  useQuery,
 };
---
+++ src/hooks/useQuery.hook.ts
@@ -0,0 +1,80 @@
+import { useQuery as _useQuery, QueryStatus as _QueryStatus } from 'react-query';
+import { dataTransformer } from '../helpers';
+import { QueryStatus } from '../enums';
+import { GraphQLContext } from '../context';
+import { useContext } from '.';
+
+/**
+ * The `useQuery` hook response.
+ * Generic type `T for the response data
+ */
+interface UseQueryResponse<T extends Record<keyof T, unknown>> {
+  readonly status: QueryStatus;
+  readonly data?: T;
+}
+
+/**
+ * Used to make a request to the GraphQL API using the
+ * `client` from the `GraphQLContext`.
+ *
+ * Generic type `T` for the response data.
+ * Generic Type `V` for the request variables.
+ *
+ * @param document The GraphQL query document
+ * @param variables The GraphQL query varibales
+ *
+ * @returns The `useQuery` hook response
+ * @example
+ *
+ * const { status, data } = useQuery<Data, Variables>(document, variables);
+ */
+const useQuery = <
+  T extends Partial<Record<keyof T, unknown>>,
+  V extends Partial<Record<keyof V, unknown>>,
+>(
+  document: string,
+  variables: V,
+): UseQueryResponse<T> => {
+
+  // Stringify and base64 encode the variables
+  // to act as the cache key for the request
+  const keyData = JSON.stringify(variables);
+  const cacheKey = Buffer
+    .from(keyData)
+    .toString('base64');
+
+  const { client } = useContext(GraphQLContext);
+  const { status, data } = _useQuery<T>({
+    queryKey: cacheKey,
+    queryFn: async () => {
+
+      // Make the GraphQL request and get
+      // the first key from the response
+      const repsonse = await client.request<{ [key: string]: T }, V>(document, variables);
+      const [key] = Object.keys(repsonse);
+
+      // Stringify the respone and pasre it
+      // with the custom response formatter
+      const text = JSON.stringify(repsonse[key]);
+      const data = JSON.parse(text, dataTransformer);
+
+      return data;
+    },
+  });
+
+  // Define the status map to map between the react-query
+  // query status and the app query status
+  const statusMap: Record<_QueryStatus, QueryStatus> = {
+    idle: QueryStatus.PENDING,
+    loading: QueryStatus.LOADING,
+    success: QueryStatus.SUCCESS,
+    error: QueryStatus.ERROR,
+  };
+
+  return {
+    status: statusMap[status],
+    data: data,
+  };
+};
+
+export default useQuery;

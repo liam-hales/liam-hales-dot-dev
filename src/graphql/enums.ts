9e8f9b9f9b1df665c5282706afcf898715ad51d2
Liam Hales <liam.hales96@gmail.com>
2022-06-09 22:39:08 +0100

    Added GraphQL enums and types and page data

---
+++ src/graphql/enums.ts
@@ -0,0 +1,11 @@
+/**
+ * The page slug used for fetching page
+ * data from the GraphQL API
+ */
+export enum PageSlug {
+  GLOBAL = 'GLOBAL',
+  HOME = 'HOME',
+  CURRICULUM_VITAE = 'CURRICULUM_VITAE',
+  BLOG = 'BLOG',
+  BRANDING = 'BRANDING',
+}
---
+++ src/graphql/types.ts
@@ -0,0 +1,69 @@
+import { PageSlug } from './enums';
+
+/**
+ * Describes the data for each page.
+ * Generic type `T` for the page slug
+ */
+export interface PageData<T extends PageSlug = never> {
+  id: string;
+  name: string;
+  slug: PageSlug;
+  content: PageContent<T>;
+}
+
+/**
+ * Describes the base content for each page.
+ * Each page content should `extends` this
+ */
+export interface BasePageContent {
+  slug: PageSlug;
+}
+
+/**
+ * Describes the global page content which
+ * is used for each page within the app
+ */
+export interface GlobalPageContent extends BasePageContent {
+  slug: PageSlug.GLOBAL;
+  footerText: string;
+  builtUsingText: string;
+  email: string;
+  linkedInUrl: string;
+  stackOverflowUrl: string;
+  buyMeCoffeeUrl: string;
+}
+
+/**
+ * Describes the home page content which
+ * is specific to the home page only
+ */
+export interface HomePageContent extends BasePageContent {
+  slug: PageSlug.HOME;
+  aboutMeText: string;
+}
+
+/**
+ * The union type for all page content types.
+ * Generic type `T` for the page slug
+ */
+export type PageContent<T extends PageSlug> =
+  T extends PageSlug.GLOBAL ? GlobalPageContent :
+    T extends PageSlug.HOME ? HomePageContent : never;
+
+/**
+ * Describes the page variables used
+ * for fetching the page data.
+ *
+ * Generic type `T` for the page slug
+ */
+export type PageVariables<T extends PageSlug> =
+  {
+    slug: T;
+  }
+  & (
+    T extends PageSlug.BLOG
+      ? {
+        blogPostSlug?: string;
+      }
+      : unknown
+  );

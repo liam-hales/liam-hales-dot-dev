9981be3573a843d5f7eecaa0f731e6e4e782c710
Liam Hales <liam.hales96@gmail.com>
2022-07-11 21:29:18 +0100

    Added script to build images

---
+++ scripts/buildImages.script.ts
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
+import sharp from 'sharp';
+
+/**
+ * Used to build the `favicon.ico` and `.png`
+ * images from the `.svg` files
+ */
+(async () => {
+  const logoIconPath = join(process.cwd(), '/src/svgs/logoIcon.svg');
+  const publicPath = join(process.cwd(), '/public');
+  const iconsPath = join(publicPath, '/icons');
+
+  // Make the /public/icons directory
+  // if it doesn't exist
+  if (existsSync(iconsPath) === false) {
+    mkdirSync(iconsPath);
+  }
+
+  // Use sharp to convert the SVG files to PNG files, resize
+  // and output them to file within the public directory
+
+  await sharp(logoIconPath)
+    .png()
+    .resize(64, 64)
+    .toFile(`${publicPath}/favicon.ico`);
+
+  await sharp(logoIconPath)
+    .png()
+    .resize(180, 180)
+    .toFile(`${iconsPath}/icon-180x180.png`);
+})();

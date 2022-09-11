dea4a5cc6b68fb829fb5ed3fe9f9f2d5be5970c7
Liam Hales <liam.hales96@gmail.com>
2022-09-11 20:30:17 +0100

    Added search input to LifeTimeline and logic to filter events based on a search URL query param

--- src/components/lifeTimeline/lifeTimeline.tsx
+++ src/components/lifeTimeline/lifeTimeline.tsx
@@ -1,8 +1,11 @@
 import { FunctionComponent, ReactElement } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { IconId } from '../../enums';
 import { PageSlug } from '../../graphql';
-import { usePageContent } from '../../hooks';
+import { usePageContent, useScreen } from '../../hooks';
 import { BaseProps } from '../../types';
 import { Timeline, TimelineEvent } from '../common';
+import { StyledSearchInput } from './lifeTimeline.styled';
 
 /**
  * The `LifeTimeline` component props
@@ -18,29 +21,65 @@ type Props = BaseProps;
  */
 const LifeTimeline: FunctionComponent<Props> = ({ className }): ReactElement<Props> => {
 
+  const [params, setParams] = useSearchParams();
+
+  const { screenSize } = useScreen();
   const { timelineEvents } = usePageContent({
     slug: PageSlug.LIFE_TIMELINE,
   });
 
   return (
-    <Timeline className={className}>
-      {
-        timelineEvents.map((event, index) => {
+    <div className={className}>
+      <StyledSearchInput
+        value={params.get('search') ?? ''}
+        placeholder="Search"
+        iconId={IconId.MAGNIFYING_GLASS}
+        onChange={(value) => {
+          setParams({
+            ...(value !== '') && {
+              search: value,
+            },
+          });
+        }}
+        screenSize={screenSize}
+      />
+      <Timeline>
+        {
+          timelineEvents
+            .filter((event) => {
+              const { title, description } = event;
 
-          // Destructure the timeline event and
-          // return the timeline event component
-          const { title, description, date } = event;
-          return (
-            <TimelineEvent
-              title={title}
-              description={description}
-              date={date}
-              first={index === 0}
-            />
-          );
-        })
-      }
-    </Timeline>
+              // Join the event title and description into one text string
+              // Lowercase the text to search and the search text itself
+              const text = `${title} ${description}`.toLowerCase();
+              const search = (params.get('search') ?? '')
+                .toLowerCase()
+                .split(' ');
+
+              // Filter out events that do not include one
+              // of the words from the search string
+              return search.every((word) => text.includes(word));
+            })
+            .map((event, index) => {
+
+              // Destructure the timeline event and get the first
+              // event from the original timeline events array
+              const { id, title, description, date } = event;
+              const [firstEvent] = timelineEvents;
+
+              return (
+                <TimelineEvent
+                  key={`life-timeline-item-${index}`}
+                  title={title}
+                  description={description}
+                  date={date}
+                  first={id === firstEvent.id}
+                />
+              );
+            })
+        }
+      </Timeline>
+    </div>
   );
 };
 

827447a9b2275877d2ea10114ae55c14ee08cbd8
Liam Hales <liam.hales96@gmail.com>
2022-10-15 12:41:15 +0100

    Added scrollTo function to useScreen hook used to scroll to a HTML element

--- src/hooks/useScreen.hook.ts
+++ src/hooks/useScreen.hook.ts
@@ -1,4 +1,5 @@
 import { useMediaQuery, useTheme } from '@mui/material';
+import { RefObject } from 'react';
 import { ScreenSize } from '../enums';
 
 /**
@@ -6,16 +7,30 @@ import { ScreenSize } from '../enums';
  */
 interface UseScreenResponse {
   readonly screenSize: ScreenSize;
+
+  /**
+   * Scrolls to a HTML element using
+   * a `ref` to said element
+   *
+   * @param ref The HTML element reference
+   * @example
+   *
+   * const ref = useRef();
+   * const { scrollTo } = useScreen();
+   *
+   * scrollTo(ref);
+   */
+  readonly scrollTo: <T extends HTMLElement>(ref: RefObject<T>) => void;
 }
 
 /**
- * Used to access values about the screen
- * such as the `screenSize`
+ * Used to access infomatin about the screen such as it's
+ * size and perform actins such as scroll
  *
  * @returns The `useScreen` hook response
  * @example
  *
- * const { screenSize } = useScreen();
+ * const { screenSize, scrollTo } = useScreen();
  */
 const useScreen = (): UseScreenResponse => {
 
@@ -24,23 +39,62 @@ const useScreen = (): UseScreenResponse => {
   const small = useMediaQuery(breakpoints.only(ScreenSize.SMALL));
   const medium = useMediaQuery(breakpoints.only(ScreenSize.MEDIUM));
 
+  /**
+   * Scrolls to a HTML element using
+   * a `ref` to said element
+   *
+   * @param ref The HTML element reference
+   * @example
+   *
+   * const ref = useRef();
+   * const { scrollTo } = useScreen();
+   *
+   * scrollTo(ref);
+   */
+  const scrollTo = <T extends HTMLElement>(ref: RefObject<T>): void => {
+    const { current } = ref;
+
+    // Check if the current element on
+    // the ref exists before using it
+    if (current == null) {
+      return;
+    }
+
+    // Calculate the position to scroll to based on
+    // the element position and the screen size
+    const offset = (small === true) ? 45 : 125;
+    const position = current.offsetTop - offset;
+
+    // Scroll to the element with
+    // the smooth behavior option
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth',
+    });
+  };
+
+  // Define the deafult response with
+  // the default screen size set to large
+  const defaultResponse: UseScreenResponse = {
+    screenSize: ScreenSize.LARGE,
+    scrollTo: scrollTo,
+  };
+
   if (small === true) {
     return {
+      ...defaultResponse,
       screenSize: ScreenSize.SMALL,
     };
   }
 
   if (medium === true) {
     return {
+      ...defaultResponse,
       screenSize: ScreenSize.MEDIUM,
     };
   }
 
-  // Return large as the default screen size
-  // if none of the screen sizes are true
-  return {
-    screenSize: ScreenSize.LARGE,
-  };
+  return defaultResponse;
 };
 
 export default useScreen;

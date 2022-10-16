226dcc6d791b806c3a1b39644764341ccc80f4d7
Liam Hales <liam.hales96@gmail.com>
2022-10-16 14:42:09 +0100

    Added SkillModal to Skills component and state to open the modal and store the selected skill

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,10 +1,10 @@
-import { FunctionComponent, ReactElement } from 'react';
+import { FunctionComponent, ReactElement, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { BoxAlignment, IconId, TextAppearance } from '../../enums';
-import { PageSlug } from '../../graphql';
+import { PageSlug, Skill } from '../../graphql';
 import { usePageContent, useScreen } from '../../hooks';
 import { BaseProps } from '../../types';
-import { SkillCard } from '..';
+import { SkillModal, SkillCard } from '..';
 import { searchFilter } from '../../helpers';
 import { StyledBox, StyledSearchInput, StyledDisclaimerText, StyledGrid } from './skills.styled';
 
@@ -22,6 +22,9 @@ type Props = BaseProps;
  */
 const Skills: FunctionComponent<Props> = ({ className }): ReactElement<Props> => {
 
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [selectedSkill, setSelectedSkill] = useState<Skill | undefined>();
+
   const [params, setParams] = useSearchParams();
   const searchText = params.get('search') ?? '';
 
@@ -31,52 +34,74 @@ const Skills: FunctionComponent<Props> = ({ className }): ReactElement<Props> =>
   });
 
   return (
-    <StyledBox
-      className={className}
-      alignment={BoxAlignment.START}
-    >
-      <StyledSearchInput
-        defaultValue={searchText}
-        placeholder="Search"
-        delay={500}
-        iconId={IconId.MAGNIFYING_GLASS}
-        onChange={(value) => {
+    <>
+      {
+        (selectedSkill != null) && (() => {
+          const { name, type, description, url, image } = selectedSkill;
+          return (
+            <SkillModal
+              open={modalOpen}
+              name={name}
+              type={type}
+              description={description}
+              url={url}
+              imageUrl={image?.url}
+              onClose={() => setModalOpen(false)}
+            />
+          );
+        })()
+      }
+      <StyledBox
+        className={className}
+        alignment={BoxAlignment.START}
+      >
+        <StyledSearchInput
+          defaultValue={searchText}
+          placeholder="Search"
+          delay={500}
+          iconId={IconId.MAGNIFYING_GLASS}
+          onChange={(value) => {
 
-          // Trim the value and set the correct query params
-          // or clearing them if the value is an empty string
-          const trimmed = value.trim();
-          setParams({
-            ...(trimmed !== '') && {
-              search: trimmed,
-            },
-          });
-        }}
-        screenSize={screenSize}
-      />
-      <StyledDisclaimerText appearance={TextAppearance.SECONDARY}>
-        {disclaimerText}
-      </StyledDisclaimerText>
-      <StyledGrid>
-        {
-          skills
-            .filter((skill) => searchFilter(searchText, skill, ['name']))
-            .map((skill, index) => {
+            // Trim the value and set the correct query params
+            // or clearing them if the value is an empty string
+            const trimmed = value.trim();
+            setParams({
+              ...(trimmed !== '') && {
+                search: trimmed,
+              },
+            });
+          }}
+          screenSize={screenSize}
+        />
+        <StyledDisclaimerText appearance={TextAppearance.SECONDARY}>
+          {disclaimerText}
+        </StyledDisclaimerText>
+        <StyledGrid>
+          {
+            skills
+              .filter((skill) => searchFilter(searchText, skill, ['name']))
+              .map((skill, index) => {
 
-              // Destructure the skill and return
-              // the skill card component
-              const { name, type, image } = skill;
-              return (
-                <SkillCard
-                  key={`skill-item-${index}`}
-                  name={name}
-                  type={type}
-                  imageUrl={image?.url}
-                />
-              );
-            })
-        }
-      </StyledGrid>
-    </StyledBox>
+                // Destructure the skill and return
+                // the skill card component
+                const { name, type, image } = skill;
+                return (
+                  <SkillCard
+                    key={`skill-item-${index}`}
+                    name={name}
+                    type={type}
+                    imageUrl={image?.url}
+                    onClick={() => {
+                      setSelectedSkill(skill);
+                      setModalOpen(true);
+                    }}
+                  />
+                );
+              })
+          }
+        </StyledGrid>
+      </StyledBox>
+    </>
   );
 };
 

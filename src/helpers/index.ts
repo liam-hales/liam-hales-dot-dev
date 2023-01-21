8410b8bef034111b7cc0836cfe660f416a43742f
Liam Hales <liam.hales96@gmail.com>
2023-01-21 02:40:45 +0000

    Added useMotion helper used to wrap a component to allow it to be animated

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,7 @@
 import withRef from './withRef.helper';
+import withMotion from './withMotion.helper';
 
 export {
   withRef,
+  withMotion,
 };
---
+++ src/helpers/withMotion.helper.tsx
@@ -0,0 +1,31 @@
+import { motion } from 'framer-motion';
+import { FunctionComponent } from 'react';
+import { BaseProps } from '../types';
+import { withRef } from '.';
+
+/**
+ * Used to wrap a component in `motion` from `framer-motion` to
+ * apply the `MotionProps` and allow the component to be animated.
+ *
+ * _**NOTE:** This also uses `withRef` which is required for `framer-motion` custom components_
+ *
+ * - Generic type `T` for the HTML element
+ * - Generic type `P` for the component props
+ *
+ * @param Component The component
+ * @returns The component with a forwarded reference
+ */
+const withMotion = <
+  T extends HTMLElement,
+  P extends BaseProps<T>,
+>(
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Component: FunctionComponent<P>,
+) => {
+
+  // Forward the component reference and turn it into a
+  // `framer-motion` component to allow it to be animated
+  return motion(withRef(Component));
+};
+
+export default withMotion;

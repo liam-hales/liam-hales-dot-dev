49c4cf19e6a6716961bc8e02a0f18257bd3e81f7
Liam Hales <liam.hales96@gmail.com>
2023-02-04 18:22:53 +0000

    Added BlogPage component used to fetch the data and render the Blog component

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from 'react';
+import { Page, blogPageQuery, SearchVariables } from '../../graphql';
+import { useQuery } from '../../hooks';
+import { PageProps, ServerComponent } from '../../types';
+import Blog from './blog';
+
+/**
+ * The entry point for the blog page route `/blog`, used to fetch the required
+ * data and render the `Blog` component passing said data as props
+ *
+ * @returns The `BlogPage` component
+ */
+const BlogPage: ServerComponent<PageProps> = async ({ searchParams = {} }): Promise<ReactElement<PageProps>> => {
+
+  const { search = '' } = searchParams;
+  const { content } = await useQuery<Page<'blog'>, SearchVariables>(blogPageQuery, {
+    variables: {
+      search: search,
+    },
+  });
+
+  return (
+    <Blog content={content} />
+  );
+};
+
+export default BlogPage;

6019495b366d1afaec7fa8eac790ad7c58831578
Liam Hales <liam.hales96@gmail.com>
2023-02-04 18:21:03 +0000

    Added Blog component used to render the blog page components

---
+++ src/app/blog/blog.tsx
@@ -0,0 +1,97 @@
+/** @jsxImportSource @emotion/react */
+
+'use client';
+
+import { FunctionComponent, ReactElement, useState } from 'react';
+import { css } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { BlogPostCard, Content, Header } from '../../components';
+import { BaseProps } from '../../types';
+import { BlogContent } from '../../graphql';
+import { useScreen } from '../../hooks';
+import { Box, SearchInput } from '../../components/common';
+
+/**
+ * The `Blog` component props
+ */
+interface Props extends BaseProps {
+  readonly content: BlogContent;
+  readonly search?: string;
+}
+
+/**
+ * Used as the entry point for the blog page. Renders the
+ * blog page components using the given `content` prop
+ *
+ * @returns The `Blog` component
+ */
+const Blog: FunctionComponent<Props> = ({ content, search = '' }): ReactElement<Props> => {
+
+  const { screenSize } = useScreen();
+  const { push } = useRouter();
+  const { posts } = content;
+
+  const [searchText, setSearchText] = useState<string>(search);
+
+  return (
+    <>
+      <Header title="Blog" />
+      <Content
+        alignment="flex-start"
+        css={css`
+          padding-top: 50px;
+          padding-bottom: 100px;
+          row-gap: 40px;
+        `}
+      >
+        <SearchInput
+          value={searchText}
+          onChange={(value) => setSearchText(value)}
+          onSearch={() => {
+
+            // Define the search query params
+            // based on the search text
+            const params = new URLSearchParams({
+              ...(searchText !== '') && {
+                search: searchText,
+              },
+            });
+
+            push(`/blog?${params.toString()}`);
+          }}
+          css={css`
+            width: ${(screenSize === 'small') ? '100%' : '400px'};
+          `}
+        />
+        <Box css={css`
+          row-gap: 22px;
+        `}
+        >
+          {
+            posts.map((post, index) => {
+
+              // Destructure the blog post and return
+              // the blog post card component
+              const { title, description, tags, author, publishedDate } = post;
+              return (
+                <BlogPostCard
+                  key={`blog-post-item-${index}`}
+                  title={title}
+                  description={description}
+                  tags={tags}
+                  author={author}
+                  publishedDate={publishedDate}
+                  css={css`
+                    height: fit-content;
+                  `}
+                />
+              );
+            })
+          }
+        </Box>
+      </Content>
+    </>
+  );
+};
+
+export default Blog;

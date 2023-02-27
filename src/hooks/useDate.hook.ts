93cccdba46d8c70d643076afffbfb240bd8e30ca
Liam Hales <liam.hales96@gmail.com>
2023-02-27 18:53:43 +0000

    Added example comment to useDate hook

--- src/hooks/useDate.hook.ts
+++ src/hooks/useDate.hook.ts
@@ -27,6 +27,12 @@ interface UseDateResponse {
  * and `relativeTime` plugins
  *
  * @returns The `useDate` hook response
+ * @example
+ *
+ * const { utc, from } = useDate();
+ *
+ * const utcDate = utc();
+ * const fromDate = from('1970-01-01T00:00:00.000Z');
  */
 const useDate = (): UseDateResponse => {
 

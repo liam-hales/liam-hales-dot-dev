2780357ccbca6d7a0305416af6b85e44996078e7
Liam Hales <liam.hales96@gmail.com>
2023-03-14 20:54:50 +0000

    Added ClickAwayListener to Modal component to close modal when clicked outisde the modal Card component

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -3,7 +3,7 @@
 'use client';
 
 import { FunctionComponent, ReactElement, ReactNode } from 'react';
-import { css } from '@mui/material';
+import { ClickAwayListener, css } from '@mui/material';
 import { useScreen } from '../../hooks';
 import { BaseProps, BoxDirection, BoxAlignment, BoxJustify } from '../../types';
 import { ColourPalette } from '../../enums';
@@ -41,50 +41,57 @@ const Modal: FunctionComponent<Props> = ({ isOpen, direction, alignment, justify
           padding-bottom: ${(screenSize === 'small') ? 128 : 0}px;
         `}
       >
-        <Card
-          direction={direction}
-          alignment={alignment}
-          justify={justify}
-          initial={{
-            y: window.innerHeight,
-          }}
-          animate={{
-            y: (isOpen === true) ? 0 : window.innerHeight,
-          }}
-          transition={{
-            y: {
-              type: (isOpen === true) ? 'spring' : 'just',
-              stiffness: 182,
-              damping: 22,
-            },
-          }}
-          onAnimationComplete={() => {
-
-            // Only call `onClosed` if the modal is open to avoid it
-            // being called for the modal open animation
-            if (isOpen === false) {
-              onClosed?.();
-            }
-          }}
-          css={css`
-            position: relative;
-            max-width: 480px;
-            padding: 28px;
-            margin-left: 20px;
-            margin-right: 20px;
-          `}
+        <ClickAwayListener onClickAway={() => {
+          if (isOpen === true) {
+            onClose();
+          }
+        }}
         >
-          <IconButton
-            id="cross"
-            colour={ColourPalette.GREY_400}
-            onClick={onClose}
+          <Card
+            direction={direction}
+            alignment={alignment}
+            justify={justify}
+            initial={{
+              y: window.innerHeight,
+            }}
+            animate={{
+              y: (isOpen === true) ? 0 : window.innerHeight,
+            }}
+            transition={{
+              y: {
+                type: (isOpen === true) ? 'spring' : 'just',
+                stiffness: 182,
+                damping: 22,
+              },
+            }}
+            onAnimationComplete={() => {
+
+              // Only call `onClosed` if the modal is closed to avoid it
+              // being called for the modal open animation
+              if (isOpen === false) {
+                onClosed?.();
+              }
+            }}
             css={css`
-              position: absolute;
-              align-self: flex-end;
+              position: relative;
+              max-width: 480px;
+              padding: 28px;
+              margin-left: 20px;
+              margin-right: 20px;
             `}
-          />
-          {children}
-        </Card>
+          >
+            <IconButton
+              id="cross"
+              colour={ColourPalette.GREY_400}
+              onClick={onClose}
+              css={css`
+                position: absolute;
+                align-self: flex-end;
+              `}
+            />
+            {children}
+          </Card>
+        </ClickAwayListener>
       </Box>
     </Backdrop>
   );

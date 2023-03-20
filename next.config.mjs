3a85a3f8da419ee33db3ca4772e27af529464538
Liam Hales <liam.hales96@gmail.com>
2023-03-20 22:23:14 +0000

    Added modularizeImports to next config to decrease compile times and bundle size

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 /**
  * The Next.js config used for advanced
  * configuration within Next.js
@@ -9,6 +11,24 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
   experimental: {
     appDir: true,
+  },
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+      preventFullImport: true,
+    },
+    '@mui/material/styles': {
+      transform: '@mui/material/styles/{{member}}',
+      preventFullImport: true,
+    },
+    'react-syntax-highlighter': {
+      transform: 'react-syntax-highlighter/dist/cjs/{{ kebabCase member }}',
+      preventFullImport: true,
+    },
+    'react-syntax-highlighter/dist/cjs/styles/prism': {
+      transform: 'react-syntax-highlighter/dist/cjs/styles/prism/{{member}}',
+      preventFullImport: true,
+    },
   },
   images: {
     remotePatterns: [

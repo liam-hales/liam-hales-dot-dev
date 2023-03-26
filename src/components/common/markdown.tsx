a5354dd6845cf85b73b8a2972ddfeb456f787d73
Liam Hales <liam.hales96@gmail.com>
2023-03-26 22:26:42 +0100

    Removed use of rehype-raw dependency

--- src/components/common/markdown.tsx
+++ src/components/common/markdown.tsx
@@ -2,9 +2,8 @@
 /* eslint-disable react/no-unstable-nested-components */
 
 import { FunctionComponent, ReactElement } from 'react';
-import ReactMarkdown from 'react-markdown';
 import { css } from '@emotion/react';
-import rehypeRaw from 'rehype-raw';
+import ReactMarkdown from 'react-markdown';
 import { BaseProps } from '../../types';
 import { ColourPalette } from '../../enums';
 import { codeLanguages } from '../../constants';
@@ -30,96 +29,94 @@ const Markdown: FunctionComponent<Props> = ({ className, children }): ReactEleme
       className={className}
       alignment="flex-start"
     >
-      <ReactMarkdown
-        rehypePlugins={[rehypeRaw]}
-        components={{
-          h1: ({ children }) => {
+      <ReactMarkdown components={{
+        h1: ({ children }) => {
+          return (
+            <Title>
+              {`${children}`}
+            </Title>
+          );
+        },
+        h2: ({ children }) => {
+          return (
+            <Text
+              isBold={true}
+              element="h2"
+              css={css`
+                font-size: 28px;
+                line-height: 122%;
+              `}
+            >
+              {children}
+            </Text>
+          );
+        },
+        p: ({ children }) => {
+          return (
+            <Text
+              colour={ColourPalette.GREY_400}
+              element="span"
+              css={css`
+                padding-top: 16px;
+                padding-bottom: 16px;
+              `}
+            >
+              {children}
+            </Text>
+          );
+        },
+        strong: ({ children }) => {
+          return (
+            <Text isBold={true}>
+              {children}
+            </Text>
+          );
+        },
+        em: ({ children }) => {
+          return (
+            <Text isItalic={true}>
+              {children}
+            </Text>
+          );
+        },
+        code: ({ className, inline, children }) => {
+
+          // Check if the code is inline and not a snippet,
+          // if so render the `CodeInline` component
+          if (inline === true) {
             return (
-              <Title>
+              <CodeInline>
                 {`${children}`}
-              </Title>
+              </CodeInline>
             );
-          },
-          h2: ({ children }) => {
-            return (
-              <Text
-                isBold={true}
-                element="h2"
-                css={css`
-                  font-size: 28px;
-                  line-height: 122%;
-                `}
-              >
-                {children}
-              </Text>
-            );
-          },
-          p: ({ children }) => {
-            return (
-              <Text
-                colour={ColourPalette.GREY_400}
-                element="span"
-                css={css`
-                  padding-top: 16px;
-                  padding-bottom: 16px;
-                `}
-              >
-                {children}
-              </Text>
-            );
-          },
-          strong: ({ children }) => {
-            return (
-              <Text isBold={true}>
-                {children}
-              </Text>
-            );
-          },
-          em: ({ children }) => {
-            return (
-              <Text isItalic={true}>
-                {children}
-              </Text>
-            );
-          },
-          code: ({ className, inline, children }) => {
+          }
 
-            // Check if the code is inline and not a snippet,
-            // if so render the `CodeInline` component
-            if (inline === true) {
-              return (
-                <CodeInline>
-                  {`${children}`}
-                </CodeInline>
-              );
-            }
+          // Get the code langauge from the class name and
+          // check for a match in the code langauges array
+          const language = codeLanguages
+            .find((value) => value === className?.replace('language-', ''));
 
-            // Get the code langauge from the class name and
-            // check for a match in the code langauges array
-            const language = codeLanguages
-              .find((value) => value === className?.replace('language-', ''));
-
-            return (
-              <CodeSnippet
-                language={language}
-                css={css`
-                  width: 100%;
-                `}
-              >
-                {`${children}`}
-              </CodeSnippet>
-            );
-          },
-          // Renders the children within a fragment in
-          // order to remove the `<pre>` tags
-          pre: ({ children }) => {
-            return (
-              <>
-                {children}
-              </>
-            );
-          },
-        }}
+          return (
+            <CodeSnippet
+              language={language}
+              css={css`
+                width: 100%;
+              `}
+            >
+              {`${children}`}
+            </CodeSnippet>
+          );
+        },
+        // Renders the children within a fragment in
+        // order to remove the `<pre>` tags
+        pre: ({ children }) => {
+          return (
+            <>
+              {children}
+            </>
+          );
+        },
+      }}
       >
         {children}
       </ReactMarkdown>

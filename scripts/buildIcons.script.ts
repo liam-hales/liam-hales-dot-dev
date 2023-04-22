c55734e4cce50ec4561ec95a0243dee6c027d5ad
Liam Hales <liam.hales96@gmail.com>
2023-04-22 14:07:49 +0100

    Updated build icons script, removed unused logic and updated path to point to the correct icon SVG

--- scripts/buildIcons.script.ts
+++ scripts/buildIcons.script.ts
@@ -1,54 +1,27 @@
-import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import sharp from 'sharp';
 
 /**
  * Used to build the `favicon.ico` and `.webp`
- * icons from the `.svg` files
+ * icons from the `icon.svg` file
  */
 (async () => {
-  const logoIconPath = join(process.cwd(), '/src/svgs/logoIcon.svg');
   const publicPath = join(process.cwd(), '/public');
-  const iconsPath = join(publicPath, '/icons');
+  const iconPath = join(publicPath, '/icon.svg');
 
-  // Make the /public/icons directory
-  // if it doesn't exist
-  if (existsSync(iconsPath) === false) {
-    mkdirSync(iconsPath);
-  }
-
-  // Define the favicon size and rect used to
-  // cut out the icon so it has rounded corners
   const faviconSize = 64;
-  const faviconRect = Buffer.from(
-    `<svg>
-      <rect
-        x="0"
-        y="0"
-        width="${faviconSize}"
-        height="${faviconSize}"
-        rx="10"
-        ry="10"
-      />
-    </svg>`,
-  );
+  const appleIconSize = 180;
 
   // Use sharp to convert the SVG files to WebP files, resize
   // and output them to file within the public directory
 
-  await sharp(logoIconPath)
+  await sharp(iconPath)
     .webp()
     .resize(faviconSize, faviconSize)
-    .composite([
-      {
-        input: faviconRect,
-        blend: 'dest-in',
-      },
-    ])
     .toFile(`${publicPath}/favicon.ico`);
 
-  await sharp(logoIconPath)
+  await sharp(iconPath)
     .webp()
-    .resize(180, 180)
-    .toFile(`${iconsPath}/icon-180x180.webp`);
+    .resize(appleIconSize, appleIconSize)
+    .toFile(`${publicPath}/apple-touch-icon.webp`);
 })();

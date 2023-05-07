cefd53729b448016af97654c21d5e2fd85e460e1
Liam Hales <liam.hales96@gmail.com>
2023-05-07 22:58:32 +0100

    Added metadataBase prop to AppLayout metadata and updated comment

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,15 +1,21 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import { Metadata } from 'next';
 import { ColourPalette } from '../enums';
+import { useConfig } from '../hooks/server';
 
 /**
- * Used to generate the `AppLayout` page component metadata
+ * Used to generate the `AppLayout` layout component metadata
  * that is rendered within the page `<head/>` element
  *
  * @returns The metadata for the `AppLayout` component
  */
 export const generateMetadata = async (): Promise<Metadata> => {
+
+  const { siteUrl } = useConfig();
   return {
     themeColor: ColourPalette.GREY_1000,
+    metadataBase: new URL(siteUrl),
     icons: {
       icon: [
         {

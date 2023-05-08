e1bc402750a7b4510dbddc07eafb2cbada7269e9
Liam Hales <liam.hales96@gmail.com>
2023-05-08 21:04:33 +0100

    Added sitemap file which exports a function used to build the sitemap.xml file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,47 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
+import { MetadataRoute } from 'next';
+import { useConfig } from '../hooks/server';
+import { useDate } from '../hooks';
+
+/**
+ * Used to build the `sitemap.xml` file to help search
+ * engine crawlers crawl your site more efficiently
+ *
+ * @returns The `sitemap.xml` file
+ */
+const buildSitemap = (): MetadataRoute.Sitemap => {
+
+  const { siteUrl } = useConfig();
+  const { utc } = useDate();
+
+  const lastModified = utc().toDate();
+  return [
+    {
+      url: siteUrl,
+      lastModified: lastModified,
+    },
+    {
+      url: `${siteUrl}/cv`,
+      lastModified: lastModified,
+    },
+    {
+      url: `${siteUrl}/cv/life-timeline`,
+      lastModified: lastModified,
+    },
+    {
+      url: `${siteUrl}/cv/skills`,
+      lastModified: lastModified,
+    },
+    {
+      url: `${siteUrl}/blog`,
+      lastModified: lastModified,
+    },
+    {
+      url: `${siteUrl}/brand`,
+      lastModified: lastModified,
+    },
+  ];
+};
+
+export default buildSitemap;

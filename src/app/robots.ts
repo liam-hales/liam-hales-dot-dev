155269ce7e9100d91a657cfcadb55eac765dbbd9
Liam Hales <liam.hales96@gmail.com>
2023-05-08 21:05:37 +0100

    Added robots file which exports a function used to build the robots.txt file

---
+++ src/app/robots.ts
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
+import { MetadataRoute } from 'next';
+import { useConfig } from '../hooks/server';
+
+/**
+ * Used to build the `robots.txt` file to tell search engine
+ * crawlers which URLs they can access on your site
+ *
+ * @returns The `robots.txt` file
+ */
+const buildRobots = (): MetadataRoute.Robots => {
+
+  const { siteUrl } = useConfig();
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
+};
+
+export default buildRobots;

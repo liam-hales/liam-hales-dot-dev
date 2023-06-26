5e65b44f9aa86b6fd754074fd8f287ecf0ab3bd9
Liam Hales <liam.hales96@gmail.com>
2023-06-26 21:02:04 +0100

    Updated build-icons script to modify the icon SVG attributes to make the icon more suitable for the apple-touch-icon

--- scripts/buildIcons.script.ts
+++ scripts/buildIcons.script.ts
@@ -1,4 +1,6 @@
 import { join } from 'path';
+import { readFileSync } from 'fs';
+import { parseStringPromise, Builder } from 'xml2js';
 import sharp from 'sharp';
 
 /**
@@ -12,6 +14,25 @@ import sharp from 'sharp';
   const faviconSize = 64;
   const appleIconSize = 180;
 
+  // Read the SVG icon data and parse it
+  // using `xml2js` so it can be modified
+  const svgData = readFileSync(iconPath, 'utf-8');
+  const iconSvg = await parseStringPromise(svgData);
+
+  // Modify the icon SVG attributes to resize the
+  // background rect and remove it's corner radius
+  iconSvg.svg.$.viewBox = '-86 -86 1196 1196';
+  iconSvg.svg.rect[0].$ = {
+    x: -86,
+    y: -86,
+    width: 1196,
+    height: 1196,
+    rx: 0,
+  };
+
+  const svgString = new Builder().buildObject(iconSvg);
+  const svgBuffer = Buffer.from(svgString);
+
   // Use sharp to convert the SVG files to WebP files, resize
   // and output them to file within the public directory
 
@@ -20,7 +41,7 @@ import sharp from 'sharp';
     .resize(faviconSize, faviconSize)
     .toFile(`${publicPath}/favicon.ico`);
 
-  await sharp(iconPath)
+  await sharp(svgBuffer)
     .webp()
     .resize(appleIconSize, appleIconSize)
     .toFile(`${publicPath}/apple-touch-icon.webp`);

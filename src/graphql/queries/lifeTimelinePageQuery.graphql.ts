fa5f4ba906a9a6f6d988529927f75cd6a2ae6b48
Liam Hales <liam.hales96@gmail.com>
2023-06-29 19:25:00 +0100

    Updated comment

--- src/graphql/queries/lifeTimelinePageQuery.graphql.ts
+++ src/graphql/queries/lifeTimelinePageQuery.graphql.ts
@@ -8,10 +8,7 @@ import {
 
 /**
  * The GraphQL query used for fetching
- * the CV life timeline page data.
- *
- * Default value required for `$search` variable due to
- * how Hygraph handles searching within GraphQL.
+ * the CV life timeline page data
  */
 const lifeTimelinePageQuery = gql`
   query lifeTimelinePage {

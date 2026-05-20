905a5f68062bbfead168246647aea61095b2f17e
Liam Hales <liam.hales96@gmail.com>
2026-05-20 17:41:39 +0100

    Added `App` component responsible for rendering the main application

---
+++ src/components/app.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent, ReactElement } from 'react';
+
+/**
+ * The component responsible for rendering the main application,
+ * its layout and any downstream child components
+ *
+ * @returns The `App` component
+ */
+const App: FunctionComponent = (): ReactElement => {
+  return (
+    <div className="flex flex-col items-center">
+    </div>
+  );
+};
+
+export default App;

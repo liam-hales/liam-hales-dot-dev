d59129d4d089ac787aeef816932f6e8c6115e522
Liam Hales <liam.hales96@gmail.com>
2026-05-21 20:34:48 +0100

    Added `Logo` component used to render the Liam Hales logo SVG

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import App from './app';
+import Logo from './logo';
 import SocialLink from './socialLink';
 import ThemeToggle from './themeToggle';
 
 export {
   App,
+  Logo,
   SocialLink,
   ThemeToggle,
 };

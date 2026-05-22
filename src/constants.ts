d1941b42d3a6ce2e86c272d748333d96e9acf251
Liam Hales <liam.hales96@gmail.com>
2026-05-22 23:09:36 +0100

    Added message suggestions array constant

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * Defines message suggestions for the user to
+ * use if they are not sure what to type
+ */
+export const suggestions = [
+  'Tell me about your experience',
+  'What\'s your tech stack?',
+  'What projects have you built?',
+  'Why should I hire you?',
+];

468b432e1ff63cd0e5e625cddc6df73e2e716cd9
Liam Hales <liam.hales96@gmail.com>
2026-05-23 19:15:51 +0100

    Updated `Welcome` component styles

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -22,25 +22,25 @@ const Welcome: FunctionComponent<Props> = ({ onSuggestion }): ReactElement<Props
   return (
     <div className="flex flex-col items-center gap-y-10">
       <Logo className="w-12 fill-accent" />
-      <div className="flex flex-col items-center">
-        <p className="text-content-primary text-[40px]">
+      <div className="flex flex-col items-center gap-y-2">
+        <p className="text-content-primary text-[40px] text-center leading-10">
           Have a chat with Liam.
         </p>
         <div className="flex flex-col items-center">
-          <p className="text-content-secondary">
+          <p className="text-content-secondary text-center">
             Senior Software Engineer from Manchester.
           </p>
-          <p className="text-content-secondary">
+          <p className="text-content-secondary text-center">
             Ask me anything or use one of the suggestions below.
           </p>
         </div>
       </div>
-      <div className="w-130 grid grid-cols-2 gap-3">
+      <div className="w-full flex flex-col sm:grid sm:grid-cols-2 gap-3">
         {
           suggestions.map((item, index) => {
             return (
               <button
-                className="flex flex-row items-center gap-x-2 border border-solid border-outline bg-surface-mid rounded-xl cursor-pointer px-4 py-3 hover:bg-surface-low"
+                className="flex flex-row items-center gap-x-2 border border-solid border-outline bg-surface-mid rounded-xl cursor-pointer px-4 py-3 hover:bg-hover"
                 key={`suggestion-${index}`}
                 onClick={() => onSuggestion(item)}
               >
@@ -48,7 +48,7 @@ const Welcome: FunctionComponent<Props> = ({ onSuggestion }): ReactElement<Props
                   className="text-content-secondary"
                   size={12}
                 />
-                <span className="text-sm text-content-primary pt-1">
+                <span className="text-sm text-content-primary pt-0.5">
                   {item}
                 </span>
               </button>

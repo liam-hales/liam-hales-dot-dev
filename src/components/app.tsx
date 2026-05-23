fd0f70a100e32d3e2798c3b70ae1035377f0bcbd
Liam Hales <liam.hales96@gmail.com>
2026-05-23 23:52:22 +0100

    Added components and logic to `App` component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { FunctionComponent, ReactElement } from 'react';
+import { Header, Welcome, Input, Option } from './';
+import { useApp } from '../hooks';
 
 /**
  * The component responsible for rendering the main application,
@@ -9,8 +11,41 @@ import { FunctionComponent, ReactElement } from 'react';
  * @returns The `App` component
  */
 const App: FunctionComponent = (): ReactElement => {
+  const {
+    inputValue,
+    mancMode,
+    messages,
+    setInputValue,
+    setMancMode,
+    sendMessage,
+  } = useApp();
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="h-full flex flex-col items-center justify-between bg-base">
+      <Header />
+      <div className="flex flex-col no-scrollbar overflow-y-auto overflow-x-hidden px-3 py-6">
+        <Welcome onSuggestion={setInputValue} />
+      </div>
+      <div className={`
+        w-full flex flex-col items-center px-3 pt-3 pb-10
+        ${(messages.length > 0) ? 'border-t border-solid border-outline' : 'border-none'}
+      `}
+      >
+        <div className="w-full max-w-175 flex flex-col items-end gap-y-3">
+          <Option
+            text="Manc mode"
+            isOn={mancMode}
+            onChange={setMancMode}
+          />
+          <Input
+            className="w-full"
+            value={inputValue}
+            status="idle"
+            onChange={setInputValue}
+            onSend={sendMessage}
+          />
+        </div>
+      </div>
     </div>
   );
 };

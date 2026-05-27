e68d11873cc42e4a4b85e634582ce5bb92d92bd8
Liam Hales <liam.hales96@gmail.com>
2026-05-27 22:14:17 +0100

    Updated `AssistantMessage` to support tool message parts

--- src/components/message/assistantMessage.tsx
+++ src/components/message/assistantMessage.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 import { FunctionComponent, ReactElement } from 'react';
-import { BaseProps } from '../../types';
-import { TextUIPart } from 'ai';
+import { BaseProps, MessagePart } from '../../types';
+import { getStaticToolName } from 'ai';
+import { Loader } from '../index';
+import { LoaderStatus } from '../types';
 
 /**
  * The `AssistantMessage` component props
  */
 interface Props extends BaseProps {
-  readonly parts: TextUIPart[];
+  readonly parts: MessagePart[];
 }
 
 /**
@@ -18,17 +22,48 @@ interface Props extends BaseProps {
  */
 const AssistantMessage: FunctionComponent<Props> = ({ className, parts }): ReactElement<Props> => {
   return (
-    <div className={`${className ?? ''} flex flex-col items-start`}>
+    <div className={`${className ?? ''} flex flex-col items-start gap-y-1`}>
       {
         parts.map((part, index) => {
-          const { text } = part;
+          const { type } = part;
+
+          // If the part it of type `text`
+          // then simply render the text
+          if (type === 'text') {
+            const { text } = part;
+            return (
+              <p
+                className="text-content-primary whitespace-pre-wrap py-3"
+                key={`assistant-message-text-part-${index}`}
+              >
+                {text}
+              </p>
+            );
+          }
+
+          // Any remaining parts are tool parts,
+          // use the tool name for the loader text
+          const { title, state } = part;
+
+          const name = title ?? getStaticToolName(part);
+          const text = `Calling tool — ${name}`;
+
+          // The map between the tool part
+          // state and the loader status
+          const statusMap: Partial<Record<typeof state, LoaderStatus>> = {
+            'output-available': 'success',
+            'output-error': 'error',
+          };
+
           return (
-            <p
-              className="text-content-primary whitespace-pre-wrap"
-              key={`assistant-message-text-part-${index}`}
+            <div
+              className="flex flex-row items-center gap-x-2"
+              key={`assistant-message-tool-part-${index}`}
             >
-              {text}
-            </p>
+              <Loader status={statusMap[state] ?? 'loading'}>
+                {text}
+              </Loader>
+            </div>
           );
         })
       }

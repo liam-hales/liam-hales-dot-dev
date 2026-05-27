74c274057d864ea664ea6b8da328b750e3b62514
Liam Hales <liam.hales96@gmail.com>
2026-05-27 22:15:49 +0100

    Updated `streamMessage` helper to only return specific message chunk types

--- src/helpers/streamMessage.helper.ts
+++ src/helpers/streamMessage.helper.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
-import { AsyncIterableStream, ModelMessage, generateId, streamText, stepCountIs } from 'ai';
+import { ModelMessage, generateId, streamText, stepCountIs } from 'ai';
 import { modelId, modelInstructions } from '../constants';
 import { tools } from '../tools';
 import { MessageChunk } from '../types';
@@ -10,9 +10,10 @@ import { MessageChunk } from '../types';
  * Used to stream the LLM message in chunks to the client using
  * the OpenRouter API and `ai` package under the hood
  *
- * @param messages The messages to send
+ * @param messages The messages to send to the model
+ * @returns The message chunk readable stream
  */
-const streamMessage = async (messages: ModelMessage[]): Promise<AsyncIterableStream<MessageChunk>> => {
+const streamMessage = async (messages: ModelMessage[]): Promise<ReadableStream<MessageChunk>> => {
   const openRouterApiKey = process.env.OPEN_ROUTER_API_KEY;
 
   // Make sure the `OPEN_ROUTER_API_KEY`
@@ -44,10 +45,53 @@ const streamMessage = async (messages: ModelMessage[]): Promise<AsyncIterableStr
     },
   });
 
-  // Convert the result into a UI message
-  // stream ready for the client to handle
-  return result.toUIMessageStream({
+  // Convert the result into a UI message stream
+  // ready for the client to handle
+  const stream = result.toUIMessageStream({
     generateMessageId: generateId,
+  });
+
+  // The array of allowed message chunk types
+  // that are safe to stream to the client
+  const allowedChunkTypes: MessageChunk['type'][] = [
+    'start',
+    'finish',
+    'abort',
+    'error',
+    'text-start',
+    'text-delta',
+    'text-end',
+    'tool-input-start',
+    'tool-input-delta',
+    'tool-input-available',
+    'tool-input-error',
+    'tool-output-available',
+    'tool-output-error',
+  ];
+
+  return new ReadableStream<MessageChunk>({
+    start: async (controller) => {
+      try {
+        for await (const chunk of stream) {
+          const { type } = chunk;
+
+          // Only update the stream
+          // with allowed message chunks
+          if (allowedChunkTypes.includes(type) === true) {
+            controller.enqueue(chunk);
+          }
+        }
+
+        // Close the stream once all message
+        // chunks have been processed
+        controller.close();
+      }
+      catch (error) {
+        // Catch any errors and
+        // update the streamable
+        controller.error(error);
+      }
+    },
   });
 };
 

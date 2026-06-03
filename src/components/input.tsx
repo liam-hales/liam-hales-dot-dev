20f390dd9e9979f75bcd45e799149345ea34627a
Liam Hales <liam.hales96@gmail.com>
2026-06-03 23:59:04 +0100

    Added support to render "Clear chat" button in `Input` component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactElement, KeyboardEvent } from 'react';
 import { BaseProps } from '../types';
-import { ArrowUp, Square } from 'lucide-react';
+import { ArrowUp, CircleX, Square } from 'lucide-react';
 import TextArea from 'react-textarea-autosize';
 
 /**
@@ -10,6 +10,7 @@ interface Props extends BaseProps {
   readonly value: string;
   readonly status: 'send' | 'abort';
   readonly onChange: (value: string) => void;
+  readonly onClear: () => void;
   readonly onSend: () => void;
   readonly onAbort: () => void;
 }
@@ -27,6 +28,7 @@ const Input: FunctionComponent<Props> = (props): ReactElement<Props> => {
     value,
     status,
     onChange,
+    onClear,
     onSend,
     onAbort,
   } = props;
@@ -54,44 +56,60 @@ const Input: FunctionComponent<Props> = (props): ReactElement<Props> => {
   };
 
   return (
-    <div className={`${className ?? ''} flex flex-row items-end gap-x-4 bg-surface-high border border-solid border-outline rounded-xl p-2`}>
-      <TextArea
-        className="w-full max-h-40 text-content-primary placeholder-content-secondary outline-none caret-content-primary resize-none px-2 py-1.5"
-        placeholder="Ask me anything..."
-        value={value}
-        onKeyDown={_onKeyDown}
-        onChange={({ target }) => onChange(target.value)}
-      />
-      {
-        (status === 'send') && (
-          <button
-            className={`
-              size-9 flex flex-col items-center justify-center shrink-0 text-white cursor-pointer bg-accent hover:bg-accent-hover rounded-lg p-2
+    <div className={`${className ?? ''} flex flex-col items-start gap-y-3 bg-surface-high border border-solid border-outline rounded-xl p-2`}>
+      <div className="flex flex-row items-center gap-x-2 p-1">
+        <button
+          className="flex flex-row items-center gap-x-1.5 bg-surface-mid border border-solid border-outline hover:bg-hover rounded-full cursor-pointer px-3"
+          onClick={onClear}
+        >
+          <CircleX
+            className="pb-px"
+            size={10}
+          />
+          <p className="text-[11px] text-content-primary pt-0.75">
+            Clear chat
+          </p>
+        </button>
+      </div>
+      <div className="w-full flex flex-row items-end gap-x-4">
+        <TextArea
+          className="w-full max-h-40 text-content-primary placeholder-content-secondary outline-none caret-content-primary resize-none px-2 py-1.5"
+          placeholder="Ask me anything..."
+          value={value}
+          onKeyDown={_onKeyDown}
+          onChange={({ target }) => onChange(target.value)}
+        />
+        {
+          (status === 'send') && (
+            <button
+              className={`
+                size-9 flex flex-col items-center justify-center shrink-0 text-white cursor-pointer bg-accent hover:bg-accent-hover rounded-lg p-2
 
-              disabled:cursor-not-allowed
-              disabled:text-content-secondary
-              disabled:bg-disabled
-            `}
-            onClick={onSend}
-            disabled={value.trim() === ''}
-          >
-            <ArrowUp size={20} />
-          </button>
-        )
-      }
-      {
-        (status === 'abort') && (
-          <button
-            className="size-9 flex flex-col items-center justify-center shrink-0 text-content-secondary cursor-pointer bg-disabled border border-solid border-outline hover:bg-hover rounded-lg p-2"
-            onClick={onAbort}
-          >
-            <Square
-              className="fill-content-secondary"
-              size={16}
-            />
-          </button>
-        )
-      }
+                disabled:cursor-not-allowed
+                disabled:text-content-secondary
+                disabled:bg-disabled
+              `}
+              onClick={onSend}
+              disabled={value.trim() === ''}
+            >
+              <ArrowUp size={20} />
+            </button>
+          )
+        }
+        {
+          (status === 'abort') && (
+            <button
+              className="size-9 flex flex-col items-center justify-center shrink-0 text-content-secondary cursor-pointer bg-disabled border border-solid border-outline hover:bg-hover rounded-lg p-2"
+              onClick={onAbort}
+            >
+              <Square
+                className="fill-content-secondary"
+                size={16}
+              />
+            </button>
+          )
+        }
+      </div>
     </div>
   );
 };

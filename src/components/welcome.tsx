ac46b5279e5cb4e6da01e6671044520772eb3778
Liam Hales <liam.hales96@gmail.com>
2026-06-06 19:07:19 +0100

    Added pagination to suggestions in `Welcome` component

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,8 +1,11 @@
-import { FunctionComponent, ReactElement } from 'react';
+'use client';
+
+import { FunctionComponent, ReactElement, useMemo, useState } from 'react';
 import { Logo } from './';
 import { suggestions } from '../constants';
-import { CircleSmall } from 'lucide-react';
+import { CircleSmall, ChevronLeft, ChevronRight } from 'lucide-react';
 import { BaseProps } from '../types';
+import { useMediaQuery } from 'usehooks-ts';
 
 /**
  * The `Welcome` component props
@@ -19,6 +22,32 @@ interface Props extends BaseProps {
  * @returns The `Welcome` component
  */
 const Welcome: FunctionComponent<Props> = ({ className, onSuggestion }): ReactElement<Props> => {
+  const [page, setPage] = useState<number>(0);
+
+  const isLargeScreen = useMediaQuery('(min-width: 640px)', {
+    initializeWithValue: false,
+  });
+
+  /**
+   * Used to calculate the page items,
+   * page count and page size
+   */
+  const [pageItems, pageCount, pageSize] = useMemo(() => {
+    const pageSize = (isLargeScreen === true) ? 4 : suggestions.length;
+    const pageCount = Math.ceil(suggestions.length / pageSize);
+
+    // Calculate the start index and slice the
+    // data array to get the page items
+    const startIndex = page * pageSize;
+    const pageItems = suggestions.slice(startIndex, startIndex + pageSize);
+
+    return [
+      pageItems,
+      pageCount,
+      pageSize,
+    ];
+  }, [page, isLargeScreen]);
+
   return (
     <div className={`${className ?? ''} flex flex-col items-center sm:justify-center gap-y-10`}>
       <Logo className="w-12 fill-accent shrink-0" />
@@ -35,25 +64,55 @@ const Welcome: FunctionComponent<Props> = ({ className, onSuggestion }): ReactEl
           </p>
         </div>
       </div>
-      <div className="w-full flex flex-col sm:grid sm:grid-cols-2 gap-3">
+      <div className="w-full flex flex-row items-center gap-x-3">
         {
-          suggestions.map((item, index) => {
-            return (
-              <button
-                className="flex flex-row items-center gap-x-2 border border-solid border-outline bg-surface-mid rounded-xl cursor-pointer px-4 py-3 hover:bg-hover"
-                key={`suggestion-${index}`}
-                onClick={() => onSuggestion(item)}
-              >
-                <CircleSmall
-                  className="text-content-secondary"
-                  size={12}
-                />
-                <span className="text-sm text-content-primary pt-0.5">
-                  {item}
-                </span>
-              </button>
-            );
-          })
+          (isLargeScreen === true) && (
+            <button
+              className="flex items-center justify-center cursor-pointer disabled:opacity-30 disabled:cursor-not-allowed"
+              disabled={page === 0}
+              onClick={() => setPage((current) => Math.max(current - 1, 0))}
+            >
+              <ChevronLeft
+                className="text-content-secondary"
+                size={22}
+              />
+            </button>
+          )
+        }
+        <div className="w-full flex flex-col sm:grid sm:grid-cols-2 gap-3">
+          {
+            pageItems.map((item, index) => {
+              return (
+                <button
+                  className="flex flex-row items-center gap-x-2 border border-solid border-outline bg-surface-mid rounded-xl cursor-pointer px-4 py-3 hover:bg-hover"
+                  key={`suggestion-${(page * pageSize) + index}`}
+                  onClick={() => onSuggestion(item)}
+                >
+                  <CircleSmall
+                    className="text-content-secondary"
+                    size={12}
+                  />
+                  <span className="text-sm text-content-primary pt-0.5">
+                    {item}
+                  </span>
+                </button>
+              );
+            })
+          }
+        </div>
+        {
+          (isLargeScreen === true) && (
+            <button
+              className="flex items-center justify-center cursor-pointer disabled:opacity-30 disabled:cursor-not-allowed"
+              disabled={page >= (pageCount - 1)}
+              onClick={() => setPage((current) => Math.min(current + 1, pageCount - 1))}
+            >
+              <ChevronRight
+                className="text-content-secondary"
+                size={22}
+              />
+            </button>
+          )
         }
       </div>
     </div>

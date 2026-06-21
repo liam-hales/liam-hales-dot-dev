cdbbb5cb4b24191a5ffa9857b54265415d2969a8
Liam Hales <liam.hales96@gmail.com>
2026-06-21 23:26:41 +0100

    Updated `Header` component to support `className` prop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,12 @@ import { FunctionComponent, ReactElement } from 'react';
 import { Logo, SocialLink, ThemeToggle } from './';
 import { useApp } from '../hooks';
 import { useMediaQuery } from 'usehooks-ts';
+import { BaseProps } from '../types';
+
+/**
+ * The `Header` component props
+ */
+type Props = BaseProps;
 
 /**
  * Used to render the header
@@ -11,7 +17,7 @@ import { useMediaQuery } from 'usehooks-ts';
  *
  * @returns The `Header` component
  */
-const Header: FunctionComponent = (): ReactElement => {
+const Header: FunctionComponent<Props> = ({ className }): ReactElement<Props> => {
   const { environment, messages } = useApp();
 
   const isLargeScreen = useMediaQuery('(min-width: 640px)', {
@@ -19,7 +25,7 @@ const Header: FunctionComponent = (): ReactElement => {
   });
 
   return (
-    <div className="w-full h-20 flex flex-row items-center justify-between border border-solid border-divider px-6 rounded-2xl">
+    <div className={`${className ?? ''} w-full h-20 flex flex-row items-center justify-between border border-solid border-divider px-6 rounded-2xl`}>
       <div className="flex flex-row items-center gap-x-8">
         {
           (messages.length > 0 && isLargeScreen === true) && (

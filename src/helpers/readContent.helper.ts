3427c6e90a790a650b655db92dcd3970caea7d80
Liam Hales <liam.hales96@gmail.com>
2026-08-14 20:03:48 +0100

    Updated `readContent` helper to only support `getTools`

--- src/helpers/readContent.helper.ts
+++ src/helpers/readContent.helper.ts
@@ -1,18 +1,24 @@
 'use server';
 
-import { tools } from '../tools';
+import { getTools } from '../tools/get';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
 /**
- * Used to read the tool content `.content.md`
- * file and return its data
+ * Used to read the `get` tool content from its
+ * `.content.md` file and return its data
  *
  * @param toolName The tool to read content for
  * @returns The `.content.md` file data
  */
-const readContent = async (toolName: keyof typeof tools): Promise<string> => {
-  const filePath = path.join(process.cwd(), `src/tools/${toolName}/${toolName}.content.md`);
+const readContent = async (toolName: keyof typeof getTools): Promise<string> => {
+  const [verb, ...rest] = toolName.split(/(?=[A-Z])/);
+
+  // Using the tool name, calculate the path
+  // segment and full file path to read
+  const segment = `${verb}/${rest.join('').toLowerCase()}`;
+  const filePath = path.join(process.cwd(), `src/tools/${segment}/${toolName}.content.md`);
+
   return await fs.readFile(filePath, 'utf-8');
 };
 
